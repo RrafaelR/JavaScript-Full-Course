96a6099f3e85911f16547cff11b747bab9c5d191
RrafaelR <augusto.ac.teixeira@gmail.com>
2022-10-18 21:48:39 -0300

    added some notes before chllenge-1

---
+++ CourseNotes/Js-Fundamentals-2.js
@@ -0,0 +1,8 @@
+// ------------- Expressions functions with arrow functions ---------------------
+
+const calAge = birthYear => 2022-birthYear;
+const calAge2 = (birthYear) =>{return 2022-birthYear} // second way to make the same thing
+
+
+console.log(calAge(2002));
+console.log(calAge2(2002));
