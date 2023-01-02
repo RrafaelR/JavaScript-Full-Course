7b643497db9e4bbe8f5c1662d60b5cdb90002b8e
Rafael Chagas <augusto.ac.teixeira@gmail.com>
2023-01-02 15:56:15 -0300

    finished error handling and timmers

--- CourseNotes/Js-Fundamentals-1.js
+++
@@ -1,5 +0,0 @@
-// ----------------------------- Ternary Operator ------------------
-const age = 23;
-//age >=18 ? console.log("i like to drink to drink wine") : console.log("I like to drink soda") -> Not so useful
-const drink = age >=18 ? "wine" : "soda";
-console.log(drink)
--- CourseNotes/Logic/error_handling.js
+++ CourseNotes/Logic/error_handling.js
@@ -1,18 +1,55 @@
-const sum = (x,y) => {
-    if(
-        typeof x !== 'number' ||
-        typeof y !== 'number'
-    ) {
-        throw new Error('x or y isn´t a number');
-    }
-
-    return x+y;
+const errorHandling = () => {
+    const sum = (x,y) => {
+        if(
+            typeof x !== 'number' ||
+            typeof y !== 'number'
+        ) {
+            throw new Error('x or y isn´t a number');
+        }
+    
+        return x+y;
+    };
+    
+    try {
+        console.log(sum(1,2));
+        console.log(sum('1',2));
+    } catch(error){
+        console.log(error);
+        console.log('Some wrong thing happend with sum function');
+    } 
 };
 
+const completeErrorHandling = () => {
+    try{
+        console.log("Abrir o arquivo");
+        console.log("manipulando o arquivo");
+        console.log(a);
+    }catch (e){
+        console.log(e);
+    } finally{
+        console.log("Sempre fecha o arquivo");
+    }
+};
+
+const returnHour = (data) => {
+    if(data && !(data instanceof Date)){
+        throw new TypeError("Esperado uma instancia do tipo Date.");
+    }
+    if(!data){
+        data = new Date();
+    }
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
+};
 try {
-    console.log(sum(1,2));
-    console.log(sum('1',2));
-} catch(error){
-    console.log(error);
-    console.log('Some wrong thing happend with sum function');
-}
+    const hora = returnHour(11);
+    console.log(hora);
+} catch (e){
+    console.log(e);
+} finally {
+    console.log('Have a good day!!');
+};
