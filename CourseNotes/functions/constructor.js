bab145fd74aa3e3df71d1eef667a4c3bb2f7d72c
Rafael Chagas <augusto.ac.teixeira@gmail.com>
2023-01-23 14:52:52 -0300

    file with constructor functions

---
+++ CourseNotes/functions/constructor.js
@@ -0,0 +1,19 @@
+// Constructor func -> object
+
+function Pessoa (nome, sobrenome){
+    //varibles creted in constructor are private;
+    const id = 1234;
+    const internMethod = () =>{
+        console.log('It´s a private method');
+    };
+
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+
+    this.method = function (){
+        console.log(this.nome + ' with a method');
+    }
+}
+
+const p1 = new Pessoa('Rafael', 'Oliveira');
+p1.method();
